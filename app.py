52f6bf01b8ff563441802c59892c8a54a33053cf
baunilha <brucalheiros@gmail.com>
2012-11-15 16:19:07 -0500

    port

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 	app.debug = True
 	
 	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
-	app.run(host='127.0.0.1', port=port)
+	app.run(host='0.0.0.0', port=port)
 
 
 
